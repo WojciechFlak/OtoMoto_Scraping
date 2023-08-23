e053637003a0fb72a25c6b4156378af0b970c009
WojciechFlak <wojciech-flak@wp.pl>
2023-08-23 21:32:04 +0200

    Page count, new layout website scraping

--- core_app.py
+++ core_app.py
@@ -3,6 +3,7 @@ import pandas
 from bs4 import BeautifulSoup
 import requests
 import time
+import re
 
 import csv
 import datetime
@@ -36,61 +37,94 @@ class Scrapper:
         soup = BeautifulSoup(response.content, 'html.parser')
 
         pagination_div = soup.find("ul", class_="pagination-list")
-
+        max_page = 1
         if pagination_div:
             # Find all links within the pagination element
             pagination_links = pagination_div.find_all("a")
 
             max_page = 0
-            for link in pagination_links:
-                try:
+            try:
+                for link in pagination_links:
                     page_number = int(link.text)
                     max_page = max(max_page, page_number)
-                except ValueError:
-                    pass
-        '''  print(f"Total number of pages: {max_page}")
-        else:
-            print("Pagination element not found")'''
+            except ValueError:
+                max_page = 1
         return max_page
+
 
 
     def scrap_to_csv(self, url, csv_file):
         self.url = url
         self.csv_file = csv_file
 
-        max_page = self.count_pages(self.url)
-        print(f"Total number of pages: {max_page}")
+        page_number = self.count_pages(self.url)
+        print(f"Total number of pages: {page_number}")
+        if page_number == 1:
+            page_number = 2
 
-        for i in range(1, max_page):
+        for i in range(1, page_number):
             url = self.url + '?page=' + str(i)
 
             page = requests.get(url)
 
             soup = BeautifulSoup(page.content, 'html.parser')
 
-            lists = soup.find_all('article', class_='ooa-2wq0y evg565y0')
-            lists2 = soup.find_all('article', class_='ooa-j8iifw evg565y0')
-            lists3 = soup.find_all('article', class_='ooa-13ci8fj evg565y0')  # wyróżnione
+            lists = soup.find_all('article', class_='ooa-1t80gpj ev7e6t818')
+            # lists2 = soup.find_all('article', class_='ooa-j8iifw evg565y0')
+            # lists3 = soup.find_all('article', class_='ooa-13ci8fj evg565y0')  # wyróżnione
+            #lists4 = soup.find_all('article', class_="ooa-1t80gpj ev7e6t818")  # wyróżnione
 
-            lists += lists2
-            lists += lists3
+            # lists += lists2
+            # lists += lists3
+            #lists +=lists4
 
-            time.sleep(2)
+            #time.sleep(2)
 
             with open(self.csv_file, 'a', encoding='utf8', newline='') as f:
                 thewriter = csv.writer(f)
                 if i == 1:
-                    header = ['Title', 'Price', 'Year_of_production', 'Milage', 'Engine', 'update_tmstmp']
+                    header = ['Title', 'Price', 'Currency', 'Year_of_production', 'Mileage', 'Engine_type', 'Engine', 'update_tmstmp']
                     thewriter.writerow(header)
                 for list in lists:
-                    title = list.find('a', href=True).string
-                    price = list.find('span', class_='ooa-1bmnxg7').string
-                    year = list.select('li', class_='ooa-1k7nwcr euadd4q0')[0].string
-                    milage = list.select('li', class_='ooa-1k7nwcr euadd4q0')[1].string
-                    engine = list.select('li', class_='ooa-1k7nwcr euadd4q0')[2].string
+                    title = list.find('h1').string
+                    price = list.find('h3', class_='ev7e6t82 ooa-bz4efo er34gjf0').string
+                    currency = list.find('p', class_='ev7e6t81 ooa-1e3jyoe er34gjf0').string
+                    year = list.find('dd', {'data-parameter': 'year'})
+                    mileage = list.find('dd', {'data-parameter': 'mileage'})
+                    engine_type = list.find('dd', {'data-parameter': 'fuel_type'})
+
+                    # In case in larger boxes this data is provided within ""
+                    try:
+                        engine = re.split("•", list.find('p').string)
+                    except:
+                        engine =re. split("•", list.find('p', class_="ev7e6t88 ooa-17thc3y er34gjf0").string)
+
                     update_tmstmp = datetime.datetime.now().strftime("%d-%m-%Y, %H:%M")
-                    info = [title, price[:-4].replace(' ', ''), year, milage[:-3].replace(' ', ''), engine, update_tmstmp]
+
+                    price = price.replace(' ', '')
+                    currency = currency.replace(' ', '')
+                    year = year.get_text(strip=True)
+
+                    # In case there is no mileage provided on the website (new car from dealer)
+                    try:
+                        mileage = mileage.get_text(strip=True).replace(' ', '').replace('km', '')
+                    except:
+                        mileage = 0
+                    engine_type = engine_type.get_text(strip=True)
+
+                    engine = engine[0].strip()
+
+
+                    info = [title,
+                            price,
+                            currency,
+                            year,
+                            mileage,
+                            engine_type,
+                            engine,
+                            update_tmstmp]
                     thewriter.writerow(info)
+
 
     def from_csv_to_db(self, csv_file):
         with open(csv_file, 'r') as csvfile:
